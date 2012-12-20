cd56ac5a8e0eb9e6d88320d90ad5a6a8a67a85fb
funtion <xieshufang76@gmail.com>
2012-12-20 17:52:18 +0800

    修改构造函数 增加applySessionID()

--- project/herald_league/herald_league/Lib/Model/UserSessionControlModel.class.php
+++ project/herald_league/herald_league/Lib/Model/UserSessionControlModel.class.php
@@ -7,32 +7,47 @@
 
 *作者：Xie
 
-*更新日期：2012.12.18
+*更新日期：2012.12.20
 
 */
     class UserSessionControl
     {
         function __construct()     //构造函数,创建cookie存储sessionID
-		{
-		}
-		
-		private $sessionID;        //session的ID
+        {
+            if( !isset(cookie('herald_seeion_id')))    //如果不存在cookie
+            {
+                $sessionID = applySessionID();
+                cookie('herald_seeion_id',$sessionID);
+            }
+        }
+        
+        private $sessionID;        //session的ID
         private $userName;         //登陆用户的用户名
-		private $cardNumber;       //登陆用户的一卡通号
-		private $xml;              //得到的xml
-		private function dealXML() //处理得到的xml
-		{
-		}
-		
-		public function isLogin()   //判断用户是否登陆,返回true或者false
-		{
-		}
-		public function getUserName() //返回用户名
+        private $cardNumber;       //登陆用户的一卡通号
+        private $xml;              //得到的xml
+        private function dealXML() //处理得到的xml
         {
-		}
-		public function getCardNumber() //返回一卡通号
+        }
+        private function applySessionID() //向服务器要session ID
         {
-		}
-		
-	}
+            $ch = curl_init('121.248.63.105/sessionservice/sessions/');
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //如果把这行注释掉的话，就会直接输出  
+            curl_setopt($ch, CURLOPT_POST,true); //使用post提交
+			$xml=curl_exec($ch);  
+            curl_close($ch); 
+        }
+        public function isLogin()   //判断用户是否登陆,返回true或者false
+        {
+        }
+        public function getUserName() //返回用户名
+        {
+        }
+        public function getCardNumber() //返回一卡通号
+        {
+        }
+        public function getSessionID() //返回sessionID
+        {
+        }
+    }
 ?>
