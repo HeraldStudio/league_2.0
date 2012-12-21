54d3eebc4f3353a79a1efc1166c6f2ad54e8a1f2
Tairy <tairyguo@gmail.com>
2012-12-21 21:11:46 +0800

    实现了评论回复信息的显示

--- project/herald_league/herald_league/Lib/Action/League/HomeAction.class.php
+++ project/herald_league/herald_league/Lib/Action/League/HomeAction.class.php
@@ -1,9 +1,9 @@
 <?php
 /*
 
-*名称：社团空间首页
+*名称：社团空间控制器类
 
-*功能: 社团空间入口
+*功能: 显示社团空间信息
 
 *作者：Tairy
 
@@ -12,6 +12,12 @@
 */
 class HomeAction extends Action
 {	
+	/*
+	
+		社团空间首页控制器函数
+	
+	*/
+	
     public function index()
 	{
 		/*获取URL参数*/
@@ -23,6 +29,13 @@ class HomeAction extends Action
 		$this -> assign('league', $league);
 		$this -> display();
     }
+	
+	/*
+	
+		社团信息控制器函数
+	
+	*/
+	
 	public function information()
 	{
 		/*获取URL参数*/
@@ -34,6 +47,13 @@ class HomeAction extends Action
 		$this -> assign('information', $information);
 		$this ->display();
 	}
+	
+	/*
+	
+		相册控制器函数
+	
+	*/
+	
 	public function album()
 	{
 		/*获取URL参数*/
@@ -42,33 +62,102 @@ class HomeAction extends Action
 		/*获取相册信息*/
 		$Album = D('League_album');
 		$album = $Album -> where('league_id ='.$leagueid) -> select();
-		$this -> assign('album', $album);
-		$looptimes = count($album);
-		//print_r($album);
-		/*获取评论信息*/
-		for( $i = 0; $i < $looptimes; $i++ )
+		$firstlooptimes = count($album);
+		
+		for( $i = 0; $i < $firstlooptimes; $i++ )
 		{
-			$comment = $this -> getComment($album[$i]['comment_id']);
-			print_r($comment);
+			/*相册基本信息*/
+			$this -> albumid = $album[$i]['id'];
+			$this -> albumname = $album[$i]['album_name'];
+			$this -> albuminfo = $album[$i]['album_info'];
+			$this -> albumcoveradd = $album[$i]['album_cover_add'];
+			
+			/*获取相册评论信息*/
+			$this -> getCommentAndAnswer( $album[$i]['id'], 2);//相册被评论类型id是2
+			
 		}
-		$this -> display();
 	}
+	
+	/*
+	
+		图片页面控制器函数
+	
+	*/
+	
 	public function picture()
 	{
 		/*获取URL参数*/
 		$albumid = intval($this -> _param('albumid'));
 		
-		/*获取相册信息*/
+		/*获取图片信息*/
 		$Picture = D('League_picture');
 		$picture = $Picture -> where('album_id='.$albumid) -> select();
-		$this -> assign('picture', $picture);
-		$this -> display();
+		$firstlooptimes = count($picture);
+		
+		/*获取相册信息*/
+		for( $i = 0; $i < $firstlooptimes; $i++ )
+		{
+			/*图片基本信息*/
+			$this -> pictureid = $picture[$i]['id'];
+			$this -> picturename = $picture[$i]['picture_name'];
+			$this -> pictureinfo = $picture[$i]['picture_info'];
+			$this -> smallpicadd = $picture[$i]['small_picture_add'];
+			$this -> largepicadd = $picture[$i]['large_picture_add'];
+			$this -> iscover = $picture[$i]['is_cover'];
+			//$this -> display();
+			/*获取图片评论信息*/
+			$this -> getCommentAndAnswer( $picture[$i]['id'], 3);//图片被评论类型id是3
+		}
 	}
-	public function getComment($commentid)
+	
+	/*
+	
+		社团交流区控制函数
+	
+	*/
+	
+	public function communion()
 	{
+		/*获取URL参数*/
+		$albumid = intval($this -> _param('leagueid'));
+		
+		/*获取评论信息*/
+		$this -> getCommentAndAnswer( $albumid, 1);//社团被评论id是1
+	}
+	
+	/*
+	
+		获取当前页面对应的评论和回复
+		
+		第一个参数是被评论者的id
+		
+		第二个参数是被评论者的类型
+	
+	*/
+	
+	public function getCommentAndAnswer( $commentedid, $commenttype )
+	{
+		/*获取评论信息*/
+
 		$Comment = M('Comment');
-		$comment = $Comment -> where('id ='.$commentid) -> select();
-		return $comment;
+		$comment = $Comment -> where('commed_id ='.$commentedid.' AND commed_type ='.$commenttype) -> select();
+		$secondlooptimes = count($comment);
+		
+		for( $j = 0; $j < $secondlooptimes; $j++ )
+		{	
+			/*评论基本信息*/
+			
+			//这里添加代码获取更详细的评论信息
+			
+			$this -> content = $comment[$j]['content'];
+			
+			/*获取该评论下对应的回复*/
+
+			$Answer = M('Answer');
+			$answer = $Answer -> where('comment_id ='.$comment[$j]['id']) -> select();
+			$this -> assign('answer', $answer);
+		}
+		$this -> display();
 	}
 }
 ?>
--- project/herald_league/herald_league/Runtime/Cache/League/c390c45869901e51235369a14a817114.php
+++ project/herald_league/herald_league/Runtime/Cache/League/c390c45869901e51235369a14a817114.php
@@ -1,3 +1,5 @@
-<?php if (!defined('THINK_PATH')) exit(); if(is_array($picture)): $i = 0; $__LIST__ = $picture;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>照片名称：<a href = "__Uploads__/<?php echo ($vo["large_picture_add"]); ?>"><?php echo ($vo["picture_name"]); ?></a><br/>
-	照片信息：<p><?php echo ($vo["picture_info"]); ?></p>
-	<a href = "__Uploads__/<?php echo ($vo["large_picture_add"]); ?>"><img src = "__Uploads__/<?php echo ($vo["small_picture_add"]); ?>"/></a><?php endforeach; endif; else: echo "" ;endif; ?>
+<?php if (!defined('THINK_PATH')) exit();?><h2>照片名称：<a href = "__Uploads__/<?php echo ($largepicadd); ?>"><?php echo ($picturename); ?></a></h2>
+<p>照片信息：<?php echo ($pictureinfo); ?></p>
+<a href = "__Uploads__/<?php echo ($largepicadd); ?>"><img src = "__Uploads__/<?php echo ($smallpicadd); ?>"/></a><br/>
+	评论：<a><?php echo ($content); ?></a><br/>
+<?php if(is_array($answer)): $i = 0; $__LIST__ = $answer;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?>回复：<a><?php echo ($vo["content"]); ?></a><br/><?php endforeach; endif; else: echo "" ;endif; ?>
