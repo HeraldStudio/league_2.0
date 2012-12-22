54854ea908231d868c937aee27367b0a21ace166
funtion <xieshufang76@gmail.com>
2012-12-22 20:07:57 +0800

    修改构造函数，dealxml

--- project/herald_league/herald_league/Lib/Model/UserSessionControlModel.class.php
+++ project/herald_league/herald_league/Lib/Model/UserSessionControlModel.class.php
@@ -16,34 +16,40 @@
         {
             if( !isset(cookie('herald_seeion_id')))    //如果不存在cookie
             {
-                $sessionID = applySessionID();
-                cookie('herald_seeion_id',$sessionID);
+                applySessionID();
             }
+            update();
 			dealXML();    //分析xml
-        }
+            cookie('herald_seeion_id',$sessionID,10800);
+		}
         
         private $sessionID;        //session的ID
         private $userName;         //登陆用户的用户名
         private $cardNumber;       //登陆用户的一卡通号
         private $xml;              //得到的xml
+        private $message;          //收到的信息
         private function dealXML() //处理得到的xml
         {
-        }
+            $xml = simplexml_load_string('message.xml');
+			$cardNumber = $xml->properties->herald.sso.studentUser.cardNumber;
+			$userName = $xml->properties->herald.sso.studentUser.fullName;
+			$sessionID = $xml->id;
+		}
         private function applySessionID() //向服务器要session ID
         {
             $ch = curl_init('121.248.63.105/sessionservice/sessions/');
             curl_setopt($ch, CURLOPT_HEADER, false);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //保存在字符串中
-            curl_setopt($ch, CURLOPT_PORT,8080);  //8080端口			
+            curl_setopt($ch, CURLOPT_PORT,8080);  //8080端口          
             curl_setopt($ch, CURLOPT_POST,true); //使用post提交
-			$xml=curl_exec($ch);  
+            $message=curl_exec($ch);  
             curl_close($ch); 
         }
         private function update()
-		{
-		    $ch = curl_init("121.248.63.105/sessionservice/sessions/$sessionID");
-		}
-		public function isLogin()   //判断用户是否登陆,返回true或者false
+        {
+            $ch = curl_init("121.248.63.105/sessionservice/sessions/$sessionID");
+        }
+        public function isLogin()   //判断用户是否登陆,返回true或者false
         {
         }
         public function getUserName() //返回用户名
