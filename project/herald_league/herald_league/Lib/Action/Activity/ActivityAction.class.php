50516947bf8801a37e04d1256ec6ff6275098abe
funtion <xieshufang76@gmail.com>
2013-01-17 02:17:26 +0800

    增加投票页，修改活动详情页

--- project/herald_league/herald_league/Lib/Action/Activity/ActivityAction.class.php
+++ project/herald_league/herald_league/Lib/Action/Activity/ActivityAction.class.php
@@ -14,6 +14,19 @@ class ActivityAction extends Action
 {
     public function index()
     {
-        $heraldSession = D('UserSessionControl'); //控制会话
+       // $heraldSession = D('UserSessionControl'); //控制会话
+        $activityID =intval( $this ->_param('activityid') ); //获取url参数
+        $activity = M('activity');
+        $result = $activity->where("id=%d",$activityID)->find(); //查询，find（）只返回第一条
+        if($result = null || $result = false)  //找不到 或者 查询失败
+        {
+            //todo show error message
+            echo "ERROR";
+        }
+        else
+        {
+            //TODO show the message
+            //$this->assign("result",$result);
+        }
     }
 }
---
+++ project/herald_league/herald_league/Lib/Action/Activity/VoteAction.class.php
@@ -0,0 +1,19 @@
+<?php
+/*
+
+*名称：投票页
+
+*功能: 显示投票详情）
+
+*作者：xie
+
+*更新日期：2013.1。17
+
+*/
+class VoteAction extends Action
+{
+    public function Index()
+    {
+
+    }
+}
