e92bbc63fea184980f533b445b56f6aa903991ab
funtion <xieshufang76@gmail.com>
2013-01-20 20:08:35 +0800

    用U函数重定向

--- project/herald_league/herald_league/Lib/Action/Activity/ActivityAction.class.php
+++ project/herald_league/herald_league/Lib/Action/Activity/ActivityAction.class.php
@@ -18,19 +18,17 @@ class ActivityAction extends Action
        // $heraldSession = D('UserSessionControl'); //控制会话
         $activityID =intval( $this ->_param('activityid') ); //获取url参数
         $activity = M('activity');
-        //$result = $activity->where("id=%d",$activityID)->find(); //查询，find（）只返回第一条
         $result = $activity->find($activityID); //读取主键为$activityID值的数据
         if($result == null || $result == false)  //找不到 或者 查询失败
         {
             // echo "ERROR";
-            display('error');
         }
         else
         {
             //var_dump($result);
             if($result[is_vote] != 0 )//是投票
             {
-                redirect("../../../Vote/index/voteid/$result[id]");//  转向投票页
+                redirect( U('Activity/Vote/index',"voteid=$result['id']")  );//  转向投票页
             }
             else
             {
