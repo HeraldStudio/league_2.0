1c63986863ea8ce224d352b590930a0e89385e98
Tairy <tairyguo@gmail.com>
2013-01-25 18:05:36 +0800

    关注类

--- project/herald_league/herald_league/Lib/Model/AttentionModel.class.php
+++ project/herald_league/herald_league/Lib/Model/AttentionModel.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * User: xie & Tairy
@@ -120,61 +119,3 @@ class AttentionModel extends Model
     }
 }
 ?>
-=======
-<?php
-/**
- * User: xie
- * Date: 13-1-25
- * 功能：关注模型
- */
-class AttentionModel extends Model
-{
-    protected $_validate = array(
-        array('user_id','number','用户无效'),
-        array('activity_id','number','活动无效'),
-        array('isleague',array(0,1),'信息无效'),
-    );
-    public function getAttentionLeague ( $userid )
-    {
-        $attentionleague = $this -> where('user_id = '.$userid.' AND isleague = 1') -> select();
-        return $attentionleague;
-    }
-
-    public function getAttentionActivity ( $userid )
-    {
-        $attentionactivity = $this -> where('user_id = '.$userid.' AND isleague = 0') -> select();
-        return $attentionactivity;
-    }
-    public function changeAttention($data,$action = 'add')
-    {
-        switch ($action)
-        {
-            case 'add':
-                if($this->where($data)->selsct())
-                    return '你已经关注';
-                else
-                {
-                    $this->create($data);
-                    return '关注成功';
-                }
-                break;
-            case 'del':
-                if($this->select($data))
-                {
-                    $this->delete($data);
-                    return '取消关注成功';
-                }
-                else
-                {
-                    return '你还未关注';
-                }
-            default:
-                {
-                    return '非法的操作';                
-                }
-        }
-
-    }
-}
-?>
->>>>>>> 4aaada73e4d319ecdd64d6392ce5ba4d1ff3bf19
