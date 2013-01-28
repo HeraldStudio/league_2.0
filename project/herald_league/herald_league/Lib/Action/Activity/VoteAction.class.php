937045a54e09d06d753d6c929fc4d7ce92ea66ad
funtion <xieshufang76@gmail.com>
2013-01-28 10:12:59 +0800

    修改了增加投票

--- project/herald_league/herald_league/Lib/Action/Activity/VoteAction.class.php
+++ project/herald_league/herald_league/Lib/Action/Activity/VoteAction.class.php
@@ -2,7 +2,7 @@
 /*
  * 名称：投票控制器
  * 作者: xie
- * 日期：2013.1.26
+ * 日期：2013.1.28
  */
 class VoteAction extends Action
 {
@@ -29,8 +29,22 @@ class VoteAction extends Action
             $uid = $heraldSession->getUserID();
             $result=$voteItem->voteIt(intval($itemid),$uid);
             //todo deal with result
-            trace('r',$result);
-            $this->display();
+            if($result==1)
+            {
+                $this->success('投票成功');
+            }
+            else
+            {
+                $message = array(
+                  -1=>'已经投过',
+                  -2=>'超过票数限制',
+                  -3=>'选项不存在',
+                  -4=>'已过期',
+                  -5=>'投票不存在',
+                  -6=>'写入数据库错误',
+                );
+                $this->error($message[$result]);
+            }
         }
     }
 }
