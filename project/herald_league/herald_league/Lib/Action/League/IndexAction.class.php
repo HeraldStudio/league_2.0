5e5b5c07bac8c199d220a0e72f1029abfa840d3a
funtion <xieshufang76@gmail.com>
2013-03-24 06:14:05 +0800

    map,map!!

--- project/herald_league/herald_league/Lib/Action/League/IndexAction.class.php
+++ project/herald_league/herald_league/Lib/Action/League/IndexAction.class.php
@@ -23,12 +23,45 @@ class IndexAction extends Action {
         $this->top = intval($this->_param('top'));
         $this->left = intval($this->_param('left'));
         $leagueInfo = D('leagueInfo');
-        $this->league = $leagueInfo->getAllLeague();
+        $street = D('Street');
+        $this->street = $street->select();
+        $league = $leagueInfo->select();
+        foreach ($league as $key => $value) {
+            $league[$key]['location_changed'] = $this->change($value['location']);
+        }
+        $this->assign('league',$league);
         $this->display();
     }
+     public function change($s)
+     {
+         $arr  = explode(';',$s);
+         $m='';
+         foreach ($arr as $key => $value) {
+             $t = explode(':',$value);
+             $t[1] = intval(str_replace('px', '', $t[1]));
 
+            switch ($key) {
+                case 0:
+                    $t[1] += 30;
+                    break;
+                case 1:
+                    $t[1] -=50;
+                    break;
+                default:
+                    $t[1] =67;
+                    break;
+            }
+             $m.= "$t[0]:$t[1]px;";
+         }  trace("$m","m");
+        return $m;
+     }
     //ONLY FOR INIT,
-    public function init()
+     public function init()
+     {
+        $this->initStreet();
+        $this->initLeague();
+     }
+    private function initLeague()
     {
         $a = array(
             'left:572px;top:590px;width:40px;height:35px;',
@@ -116,11 +149,74 @@ class IndexAction extends Action {
             'left:1070px;top:1600px;width:40px;height:35px;',
             );
             $leagueInfo = D('LeagueInfo');
+            $leagueInfo ->query('delete  from lg_league_info');
             //$leagueInfo->addAll($a);
             foreach ($a as $key => $value) {
                 $leagueInfo->add(array('location'=>$value));
                 //$leagueInfo->addAll($a);
             }
+    }
+    private function initStreet()
+    {
+        $a = array(
+   array('location'=>'left:355px;top:250px;width:16px;height:110px;','street_name'=>'云南路','street_type'=>'',),
+   array('location'=>'left:355px;top:810px;width:16px;height:110px;','street_name'=>'上海路','street_type'=>'',),
+   array('location'=>'left:355px;top:1395px;width:16px;height:110px;','street_name'=>'上海路','street_type'=>'',),
+   array('location'=>'left:355px;top:1910px;width:16px;height:110px;','street_name'=>'莫愁路','street_type'=>'',),
+   array('location'=>'left:857px;top:250px;width:16px;height:110px;','street_name'=>'中央路','street_type'=>'',),
+   array('location'=>'left:857px;top:810px;width:16px;height:110px;','street_name'=>'鼓楼隧道','street_type'=>'',),
+   array('location'=>'left:857px;top:1395px;width:16px;height:110px;','street_name'=>'中山路','street_type'=>'',),
+   array('location'=>'left:857px;top:1910px;width:16px;height:110px;','street_name'=>'中山南路','street_type'=>'',),
+   array('location'=>'left:1520px;top:250px;width:16px;height:110px;','street_name'=>'进香河路','street_type'=>'',),
+   array('location'=>'left:1520px;top:810px;width:16px;height:110px;','street_name'=>'进香河路','street_type'=>'',),
+   array('location'=>'left:1520px;top:1395px;width:16px;height:110px;','street_name'=>'洪武北路','street_type'=>'',),
+   array('location'=>'left:1520px;top:1910px;width:16px;height:110px;','street_name'=>'洪武路','street_type'=>'',),
+   array('location'=>'left:2022px;top:250px;width:16px;height:110px;','street_name'=>'太平北路','street_type'=>'',),
+   array('location'=>'left:2022px;top:810px;width:16px;height:110px;','street_name'=>'太平北路','street_type'=>'',),
+   array('location'=>'left:2022px;top:1395px;width:16px;height:110px;','street_name'=>'太平北路','street_type'=>'',),
+   array('location'=>'left:2022px;top:1910px;width:16px;height:110px;','street_name'=>'太平北路','street_type'=>'',),
+   array('location'=>'left:520px;top:564px;width:200px;height:16px;','street_name'=>'北京西路','street_type'=>2),
+   array('location'=>'left:1100px;top:564px;width:200px;height:16px;','street_name'=>'北京东路','street_type'=>2),
+   array('location'=>'left:520px;top:1149px;width:200px;height:16px;','street_name'=>'广东路','street_type'=>2),
+   array('location'=>'left:1100px;top:1149px;width:200px;height:16px;','street_name'=>'珠江路','street_type'=>2),   
+   array('location'=>'left:520px;top:1715px;width:200px;height:16px;','street_name'=>'汉中路','street_type'=>2),
+   array('location'=>'left:1100px;top:1715px;width:200px;height:16px;','street_name'=>'中山东路','street_type'=>2),
+   array('location'=>"left:453px;top:732px;width:200px;height:16px;",'street_name'=>'长江路','street_type'=>3),
+   array('location'=>"left:933px;top:732px;width:200px;height:16px;",'street_name'=>'长江路','street_type'=>3),
+   array('location'=>"left:1313px;top:732px;width:200px;height:16px;",'street_name'=>'长江路','street_type'=>3),
+   array('location'=>"left:453px;top:1316px;width:200px;height:16px;",'street_name'=>'清水亭路','street_type'=>3),
+   array('location'=>"left:933px;top:1316px;width:200px;height:16px;",'street_name'=>'清水亭路','street_type'=>3),
+   array('location'=>"left:1313px;top:1316px;width:200px;height:16px;",'street_name'=>'清水亭路','street_type'=>3),
+   array('location'=>"left:353px;top:900px;width:200px;height:16px;",'street_name'=>'墨香路','street_type'=>3),
+   array('location'=>"left:1353px;top:900px;width:200px;height:16px;",'street_name'=>'应天巷','street_type'=>3),
+   array('location'=>"left:503px;top:1062px;width:200px;height:16px;",'street_name'=>'龙蟠路','street_type'=>3),
+   array('location'=>"left:846px;top:1062px;width:200px;height:16px;",'street_name'=>'砂珠巷','street_type'=>3),
+   array('location'=>"left:1280px;top:1062px;width:200px;height:16px;",'street_name'=>'竹山路','street_type'=>3),
+   array('location'=>"left:433px;top:1647px;width:200px;height:16px;",'street_name'=>'青云巷','street_type'=>3),
+   array('location'=>"left:673px;top:1647px;width:200px;height:16px;",'street_name'=>'青云巷','street_type'=>3),
+   array('location'=>"left:1269px;top:1647px;width:200px;height:16px;",'street_name'=>'明瓦廊','street_type'=>3),
+   array('location'=>"left:1523px;top:1647px;width:200px;height:16px;",'street_name'=>'明瓦廊','street_type'=>3),
+   array('location'=>"left:684px;top:1480px;width:200px;height:16px;",'street_name'=>'渊声巷','street_type'=>3),
+   array('location'=>"left:864px;top:1480px;width:200px;height:16px;",'street_name'=>'渊声巷','street_type'=>3),
+   array('location'=>"left:1160px;top:498px;width:200px;height:16px;",'street_name'=>'虎踞路' ,'street_type'=>3),
+   array('location'=>"left:1025px;top:380px;width:16px;height:200px;",'street_name'=>'丹凤街','street_type'=>4),  
+   array('location'=>"left:1025px;top:600px;width:16px;height:200px;",'street_name'=>'丹凤街','street_type'=>4),
+   array('location'=>"left:1354px;top:810px;width:16px;height:200px;",'street_name'=>'成贤街','street_type'=>4),
+   array('location'=>"left:1354px;top:1410px;width:16px;height:200px;",'street_name'=>'三步二桥','street_type'=>4),   
+   array('location'=>"left:695px;top:1440px;width:16px;height:200px;",'street_name'=>'桃叶渡','street_type'=>4),      
+   array('location'=>"left:522px;top:1524px;width:16px;height:200px;",'street_name'=>'永庆巷','street_type'=>4), 
+   array('location'=>"left:522px;top:924px;width:16px;height:200px;",'street_name'=>'墨香路','street_type'=>4),
+   array('location'=>"left:695px;top:854px;width:16px;height:200px;",'street_name'=>'御道街','street_type'=>4),
+   array('location'=>"left:695px;top:654px;width:16px;height:200px;",'street_name'=>'御道街','street_type'=>4),   
+   array('location'=>"left:1025px;top:1000px;width:16px;height:200px;",'street_name'=>'和燕路','street_type'=>4),
+   array('location'=>"left:1185px;top:1190px;width:16px;height:200px;",'street_name'=>'竹山路','street_type'=>4),
+);
+$street = D('Street');
+$street->query('delete  from lg_street');
+foreach ($a as $key => $value) {
+                $street->add($value);
+            }
+   
     }
 }
 ?>
